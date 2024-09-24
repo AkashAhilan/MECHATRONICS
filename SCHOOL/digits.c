0a24c55e67cf085e349fe52a3870f0078366b915
AkashAhilan <173340746+AkashAhilan@users.noreply.github.com>
2024-09-24 13:03:14 -0400

    Organinizing files

--- SCHOOL/digits.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int num = 3; 
-    double value = 10/num;  // 3.333333...
-    printf("The value: %0.2lf\n", value);
-    
-}
--- SCHOOL/testing/digits.c
+++ SCHOOL/testing/digits.c
@@ -2,10 +2,7 @@
 
 int main(){
     int num = 3; 
-    int age;
     double value = 10/num;  // 3.333333...
     printf("The value: %0.2lf\n", value);
-    scanf("%d", &age);
-    printf("%d", age);
-    return 0;
+    
 }
